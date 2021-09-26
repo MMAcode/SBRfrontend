c53e3037edf855a3cfb43d3079f624ca63965a28
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-09-26 20:55:34 +0100

    (main): event propagation stopped

--- src/Components/quizzes/QuizHead.js
+++ src/Components/quizzes/QuizHead.js
@@ -7,10 +7,14 @@ export default function QuizHead(props) {
     useEffect(()=>{
         console.log("reorder questions change registered",reorderHandler[0])
     },[reorderHandler])
+    const reorder = (e)=> {
+        e.stopPropagation();
+        reorderHandler[1](before => !before)
+    }
     return (
         <div className="quizHead">
             <h2>{firstCapital(quiz.title)}</h2>
-            <span className="restrict_admin"><button onClick={()=>reorderHandler[1](before=>!before)}>Re-order Questions</button></span>
+            <span className="restrict_admin"><button onClick={reorder}>Re-order Questions</button></span>
 
         </div>
     );
