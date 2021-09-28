f4b3a74c810fcb9b021246b7593eff94ca6b9aca
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-09-28 10:51:17 +0100

    (main): edit choice option added

--- src/Components/quizzes/quiz/question/choice/EditChoice.js
+++ src/Components/quizzes/quiz/question/choice/EditChoice.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {quizContextSource} from "../../QuizQuestions";
 import {questionContextSource} from "../../Question";
 import {choiceContextSource} from "./Choice";
@@ -7,6 +7,7 @@ export default function EditChoice() {
     const [quiz,,setQuiz] = React.useContext(quizContextSource);
     const [question] = React.useContext(questionContextSource);
     const [choice,choicePositionFrom0] = React.useContext(choiceContextSource);
+    const choiceEditHandler = useState(false);
 
     const flipCorrectAnswer = ()=> {
         choice.correctAnswer = !choice.correctAnswer;
@@ -19,12 +20,15 @@ export default function EditChoice() {
         question.choices.splice(choicePositionFrom0,1);
         setQuiz({...quiz});
     }
+    const editChoice = (newValue)=>{
+        choice.title = newValue;
+        setQuiz({...quiz});
+    }
     return (
         <div className="restrict_admin">
-            <button onClick={()=>flipCorrectAnswer()}>Change Answer
-                {/*qz{quiz.id}q{choice.question_id}o{choice.id}*/}
-            </button>
-            <button onClick={()=>deleteChoice()}>Delete Choice</button>
+            <div><input type="text" onChange={e=>editChoice(e.target.value)} defaultValue={choice.title}/></div>
+            <button onClick={flipCorrectAnswer}>Change Answer</button>
+            <button onClick={deleteChoice}>Delete Choice</button>
         </div>
     );
 }
