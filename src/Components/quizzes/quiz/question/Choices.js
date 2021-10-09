df03e4bfff7211e5109a365ec28e1f5decf5531a
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-10-09 19:34:30 +0100

    (main): all detailed styling of inputs and buttons finished

--- src/Components/quizzes/quiz/question/Choices.js
+++ src/Components/quizzes/quiz/question/Choices.js
@@ -5,13 +5,13 @@ import EditAddChoice from "./EditAddChoice";
 
 export default function Choices({data}) {
     return (
-            <div
-                className="choices"
-            >
-                {data.map((choice,positionFrom0) => <Choice data={[choice,positionFrom0]} key={positionFrom0}/>)}
-                {/*<RestrictedAccess>*/}
-                    <EditAddChoice/>
-                {/*</RestrictedAccess>*/}
+        <>
+            <div className="choices">
+                {data.map((choice, positionFrom0) =>
+                    <Choice data={[choice, positionFrom0]} key={positionFrom0}/>
+                )}
             </div>
+            <EditAddChoice/>
+        </>
     );
 }
